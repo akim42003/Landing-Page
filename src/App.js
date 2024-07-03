a01bd4af8c9670e1a20993684562c69fe74b61ad
Alexander Kim <alex@Alexanders-MBP.fios-router.home>
2024-07-02 21:18:27 -0400

    app.js hash router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
