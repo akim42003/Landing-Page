44d378ff228bedd916080013d80aec7d72d0f9e8
akim42003 <akim42003@gmail.com>
2026-03-03 21:12:08 -0600

    resizing image stuff

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,22 +1,15 @@
 <script>
     import { base } from "$app/paths";
     import { slide } from "svelte/transition";
-    import { onMount } from "svelte";
 
     let showMore = false;
-    let collapsedHeight = 0;
-    let contentEl;
-
-    onMount(() => {
-        collapsedHeight = contentEl.clientHeight;
-    });
 </script>
 
 <div class="container about fade-in">
     <h2>About</h2>
 
     <div class="about-grid">
-        <div class="about-content" bind:this={contentEl}>
+        <div class="about-content">
             <p>
 		I earned my BA in Mathematics from <strong>Hamilton College</strong> in 2025. Some of my favorite courses were Graph Theory, Modern Algebra and Advanced Linear Algebra. As an undergrad, I was a research intern at the <a href = "https://www.musculoskeletal.ai/">Musculoskeletal Informatics Group</a> at <strong>Boston Children's Hospital</strong> led by Dr. Ata Kiapour, and lead an indepdentent research project for the music department as an <strong> Emerson Fellow</strong> in 2024. 
             </p>
@@ -71,7 +64,7 @@
             </button>
         </div>
 
-        <div class="about-image-container" style="height: {collapsedHeight || 'auto'}px;">
+        <div class="about-image-container">
             <img src="{base}/selfie.jpeg" alt="Alex Kim" class="about-image" />
             <p class="image-caption">DGX Station at BCH MIG 2021</p>
         </div>
